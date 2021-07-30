edfd7f90220758d31a1240a7e3c6d1c63f40f0d0
Xronier <45444767+Xronier@users.noreply.github.com>
2021-07-29 21:22:26 -0700

    reddit fixes

--- Lib/cogs/MiscCommands.py
+++ Lib/cogs/MiscCommands.py
@@ -12,16 +12,31 @@ class MiscCommands(commands.Cog):
     async def sponge(self, ctx):
         reddit = asyncpraw.Reddit(client_id="XlF4b8x_KSv3FVW8du3F7Q",
                                   client_secret="ZzpVbtdbLcfQZOyGPa8n6SjtUTzWrw",
-                                  user_agent="Sakuranbot")
+                                  user_agent="Sakuranbot",)
 
         sponge_submissions = await reddit.subreddit('BikiniBottomTwitter')
-        sub = random.choice([meme async for meme in sponge_submissions.top(time_filter="month", limit=25)])
-
+        sub = random.choice([meme async for meme in sponge_submissions.top(time_filter="month", limit=50)])
+        print(sub.permalink)
         embed = discord.Embed(title="{}".format(sub.title), description="Author: u/{}".format(sub.author.name),
-                              color=discord.Color.gold())
+                              color=discord.Color.gold(), url="https://reddit.com{}".format(sub.permalink))
         embed = embed.set_image(url=sub.url)
+
+        sub.comment_sort = 'best'
+        sub.comment_limit = 3  # Not sure if more than 1 comment can be stickied, so select first 3
+        top_comment = "No comments exists on this post."
+        comments = await sub.comments()
+
+        if len(comments) > 0:
+            for i in range(3):
+                if comments[i].distinguished != "moderator":
+                    top_comment = comments[i].body
+                    break
+
+        embed = embed.add_field(name="Top Comment", value=top_comment)
         embed = embed.set_footer(text="👍 {} 💬 {}".format(sub.score, sub.num_comments))
         await ctx.send(embed=embed)
+        await reddit.close()
+
 
 def setup(client):
     client.add_cog(MiscCommands(client))
